b35b27af7acd807ef4478ab1fe998f1787368341
Pavel <deectro@gmail.com>
2022-06-06 10:08:10 +0300

    Change adminlte menu config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Support\ServiceProvider;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +24,17 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Dispatcher $events)
     {
-        //
+        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
+            $event->menu->add('MAIN NAVIGATION');
+            $event->menu->add([
+                'text'        => 'Users',
+                'url'         => 'admin/users',
+                'icon'        => 'users',
+                'label'       => User::count(),
+                'label_color' => 'success',
+            ]);
+        });
     }
 }
